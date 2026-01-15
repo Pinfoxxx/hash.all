f4fe1eb614882f175813671f5bb22100a2e7affc
Pinfoxxx <zhirileev.k@gmail.com>
2026-01-15 18:43:54 +0300

    modified app.py: removed non-secure logic

--- gui_v2/app.py
+++ gui_v2/app.py
@@ -3,7 +3,6 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from gui_v2.styles import STYLES
-from gui_v2.login_window import LoginWindow
 from gui_v2.main_window import MainWindow
 
 
@@ -14,14 +13,6 @@ class Runtime:
         self.app = QApplication(sys.argv)
         self.app.setStyleSheet(STYLES)
 
-        # Initializing login window and waiting success signal
-        self.login_window = LoginWindow()
-        self.login_window.success.connect(self.show_main_window)
-        self.login_window.show()
-
-    def show_main_window(self):
-        # Closing login window and show main window
-        self.login_window.close()
         self.main_window = MainWindow()
         self.main_window.show()
 
