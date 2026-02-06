2bdb51cdd9bca22d933ac366f806e2f470317b24
Pinfoxxx <zhirileev.k@gmail.com>
2026-02-06 20:04:47 +0300

    modified config.py: added some functional

--- gui_v2/config.py
+++ gui_v2/config.py
@@ -1,3 +1,5 @@
+import json
+import os
 from pathlib import Path
 from typing import ClassVar
 
@@ -26,6 +28,7 @@ class AppConfig:
     APP_NAME: ClassVar[str] = "hash.all"  # App name
     VERSION: ClassVar[str] = "0.5"  # App version
     DEFAULT_WINDOW_SIZE: ClassVar[str] = "800x600"  # App window_size
+    LANGUAGE: ClassVar[str] = "English"
 
     # File permissions / only owner
     SECURE_FILE_MODE: ClassVar[int] = 0o600
@@ -36,3 +39,42 @@ class AppConfig:
 
     # Bypass settings
     YANDEX_DIR: ClassVar[str] = "https://disk.yandex.ru/d/O22Pp0Anlf0rRA"
+
+
+def load_config():
+    "Load configation from file or create config file"
+    config_path = "settings.json"
+    if os.path.exists(config_path):
+        try:
+            with open(config_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+
+                # Loading AppConfig
+                app_data = data.get("AppConfig", {})
+                AppConfig.HIBP_REQUEST_DELAY = app_data.get(
+                    "HIBP_REQUEST_DELAY", AppConfig.HIBP_REQUEST_DELAY
+                )
+                AppConfig.HIBP_TIMEOUT = app_data.get(
+                    "HIBP_TIMEOUT", AppConfig.HIBP_TIMEOUT
+                )
+                AppConfig.LANGUAGE = app_data.get("LANGUAGE", AppConfig.LANGUAGE)
+
+                # Loading SecurityConfig
+                sec_data = data.get("SecurityConfig", {})
+                SecurityConfig.PBKDF2_ITERATIONS = sec_data.get(
+                    "PBKDF2_ITERATIONS", SecurityConfig.PBKDF2_ITERATIONS
+                )
+                SecurityConfig.BCRYPT_ROUNDS = sec_data.get(
+                    "BCRYPT_ROUNDS", SecurityConfig.BCRYPT_ROUNDS
+                )
+                SecurityConfig.SALT_SIZE = sec_data.get(
+                    "SALT_SIZE", SecurityConfig.SALT_SIZE
+                )
+                SecurityConfig.LOCKOUT_DURATION = sec_data.get(
+                    "LOCKOUT_DURATION", SecurityConfig.LOCKOUT_DURATION
+                )
+        except Exception as e:
+            print(f"Configuration saving error: {e}")
+
+
+load_config()
